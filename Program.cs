a40096779a6d665a493a608a42cab4586aaa1291
Veysi Öcal <veysiocal@hotmail.com>
2022-09-03 18:57:54 +0300

    mini-project

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Veysi", 1000);
+            Console.WriteLine( $" account number: {account.Number} , account owner: {account.Owner}, account balance: {account.Balance} ");
+            
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine("Your new balance is: " + account.Balance);
+
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+            // Test that the initial balacnes must be positive 
+            try
+            {
+                var invalidAccount = new BankAccount("incalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e) 
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+             // This catch does not stop program, just console error and continue.
+
+            account.MakeWithdrawal(10, DateTime.Now, "Xbox Game");
+            Console.WriteLine("Your new balance after xbox: " + account.Balance);
+
+            // Test for negative balance:
+            try
+            {
+                account.MakeWithdrawal(7500, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+                Console.WriteLine("geldi mi??");
+
+
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,23 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Text;
+
+namespace MySuperBank
+{
+    public class Transaction
+    {
+        public decimal Amount {  get; }
+
+        public DateTime Date { get; }
+
+        public string Notes { get; }
+
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
